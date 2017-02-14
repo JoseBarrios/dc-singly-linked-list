8dead8fe5d212474f362f567b0744708d30aecd3
Jose Barrios <jose@cognilab.com>
2017-02-14 09:53:04 -0800

    Basic functionality and unit tests are up and running (but this time I actually added the files to git)

---
+++ index.js
@@ -0,0 +1,113 @@
+'use strict'
+
+
+class SinglyLinkedList {
+
+  static Node(data){
+    let node = {};
+    node.data = data || null;
+    node.next = null;
+    return node;
+  }
+
+
+  constructor(node){
+    this.head = node || SinglyLinkedList.Node(0);
+    this.length = 0;
+  }
+
+
+
+  insert(data, index){
+    index = index || null;
+
+    let newNode = SinglyLinkedList.Node(data);
+    let currentNode = this.head;
+    //Empty list
+    if(!currentNode){
+      this.head = newNode;
+      this.length++;
+    }
+    //Non-empty list
+    else if(index){
+      let previous = this.getNodeAtIndex(index-1)
+      let nextNode = previous.next;
+      previous.next = newNode;
+      newNode.next = nextNode;
+      this.length++;
+    }
+    else{
+      let lastNode = this.getLastNode();
+      lastNode.next = newNode;
+      this.length++;
+    }
+
+    return newNode;
+  }
+
+  print(){
+    console.log(JSON.stringify(this, null, ' '))
+  }
+
+
+  deleteAtIndex(index){
+    let prev = this.getNodeAtIndex(index-1);
+    let target = this.getNodeAtIndex(index);
+    let newNext = target.next;
+    prev.next = newNext;
+    target.next = null;
+    return target;
+  }
+
+
+
+  search(targetData){
+    let targetNode = null;
+    let currentNode = this.head;
+    while(currentNode.next){
+      currentNode = currentNode.next;
+      if(currentNode.data === targetData){
+        targetNode = currentNode;
+        break;
+      }
+    }
+    return targetNode;
+  }
+
+
+
+
+
+  //HELPERS
+  getLastNode(){
+    let currentNode = this.head;
+    while(currentNode.next){ currentNode = currentNode.next; }
+    return currentNode;
+  }
+
+  getLength(){
+    let length = 0;
+    let currentNode = this.head;
+    while(currentNode.next){
+      currentNode = currentNode.next;
+      length++;
+    }
+    return length;
+  }
+
+  getNodeAtIndex(i){
+    let currentNode = this.head;
+    while(currentNode.next && i > 0){
+      currentNode = currentNode.next;
+      i--;
+    }
+    return currentNode;
+  }
+
+
+
+
+}
+
+module.exports = SinglyLinkedList;
+
---
+++ tests/singlyListTest.js
@@ -0,0 +1,72 @@
+'use strict'
+
+var SinglyLinkedList = require('../index.js');
+var assert = require('assert');
+
+var list = new SinglyLinkedList(0)
+let node1 = list.insert(1);
+//let node2 = list.insert(2);
+//let node3 = list.insert(3);
+let node4 = list.insert(4);
+let node5 = list.insert(5);
+
+//Correct outputs
+//const CORRECT_MAX_DEPTH = 3;
+
+
+describe('SinglyLinkedList', function() {
+
+  describe('#insert()', function() {
+    it('should add a node to the list', function() {
+      assert.deepStrictEqual(list.insert(6), list.getNodeAtIndex(list.getLength()));
+      assert.deepStrictEqual(list.insert(7), list.getNodeAtIndex(list.getLength()));
+      assert.deepStrictEqual(list.insert(8), list.getNodeAtIndex(list.getLength()));
+      assert.deepStrictEqual(list.insert(9), list.getNodeAtIndex(list.getLength()));
+      assert.deepStrictEqual(list.insert(10), list.getNodeAtIndex(list.getLength()));
+      assert.deepStrictEqual(list.insert(2, 2), list.getNodeAtIndex(2));
+      assert.deepStrictEqual(list.insert(3, 3), list.getNodeAtIndex(3));
+
+    });
+  });
+
+
+  describe('#search()', function() {
+    it('should get the node with the indicated value', function() {
+      assert.deepStrictEqual(list.search(5), list.getNodeAtIndex(5));
+      assert.deepStrictEqual(list.search(6), list.getNodeAtIndex(6));
+    });
+  });
+
+
+  describe('#getNodeAtIndex()', function() {
+    it('should get the node at the indicated index', function() {
+      assert.deepStrictEqual(list.getNodeAtIndex(3), list.search(3));
+    });
+  });
+
+  describe('#getLength()', function() {
+    it('should return number of nodes in the list', function() {
+      assert.deepStrictEqual(list.getLength(), 10);
+    });
+  });
+
+
+
+  describe('#deleteAtIndex()', function() {
+    it('should delete the node with the indicated index', function() {
+      let copy = list.search(5);
+      assert.deepStrictEqual(list.deleteAtIndex(5), copy);
+    });
+  });
+
+
+  describe('#getLastNode()', function() {
+    it('should return the last node in the list', function() {
+      assert.deepStrictEqual(list.getLastNode(), list.getNodeAtIndex(10));
+    });
+  });
+
+
+
+});
+
